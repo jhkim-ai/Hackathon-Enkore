c0b3c7e9fb73cfc3d2034c3cdb7ff2e1108b916d
gom626 <gom626@gmail.com>
2023-02-05 20:22:34 +0900

    마이페이지 수정사항반영
    2월5일 작업내용
    - 기부하기 버튼 클릭시 해당 화면 위치에 따라 버튼이 안먹는 경우가 있음

--- src/components/DonatePopUp.vue
+++ src/components/DonatePopUp.vue
@@ -10,7 +10,10 @@
                             <legend class="screen_out">기부금액 입력 양식</legend>
                             <div data-tiara-layer="donation donation_detail" class="wrap_fund">
                                 <dl class="list_pay">
-                                    <v-autocomplete :items="people" item-text="name" item-value="name" v-model="friends" return-object></v-autocomplete>
+                                    <div>
+                                        <v-autocomplete :items="selStock" item-text="name" deletable-chips return-object @change="(items) => checkStock(items)"></v-autocomplete>
+                                        <v-checkbox v-model="checkbox1" color="success" :label="`${curStock} 현재 기부가능 주식 총 수 : ${curStockCnt.toString()}주`" value disabled></v-checkbox>
+                                    </div>
                                     <dt class="cate_write cate_fund"><span>기부금 결제</span></dt>
                                     <dd class="info_write info_fund">
                                         <p class="txt_info"> 무통장은 3천원, 기타 결제는 1천원부터 가능합니다. </p>
@@ -49,7 +52,7 @@
                                 <div class="box_select"><span data-tiara-layer="donation donation_detail secret" data-tiara-action-name="익명기부_클릭" class="choice_comm choice_checkbox"><input type="checkbox" id="checkAnonymous" name="anonymous" class="inp_comm ng-untouched ng-pristine ng-valid" value="true"><label for="checkAnonymous" class="lab_comm"><span class="ico_together ico_checkbox"></span>익명으로 기부하기 </label></span><span class="choice_comm choice_checkbox"><input type="checkbox" id="checkShareStory" name="share_story" class="inp_comm ng-untouched ng-pristine ng-valid"></span></div>
                                 <p class="txt_msg"> 결제 수수료는 카카오가 대신 부담합니다. <br>결제완료 알림은 카카오톡으로 발송해드려요. </p>
                             </div>
-                            <div class="wrap_btn wrap_btn_type"><button type="button" freezebutton="" class="btn_set btn_type1" @click="subDonate"> 결제하기 </button></div>
+                            <div class="wrap_btn wrap_btn_type"><button type="button" freezebutton="" class="btn_set btn_type1" @click="subDonate"> 기부하기 </button></div>
                         </fieldset>
                     </form>
                 </div>
@@ -65,29 +68,40 @@
 </template>
 
 <script>
-
 import stockJSON from "@/assets/text/stockJSON.json";
 
 export default {
-    name: 'app',
+    name: 'DonatePopUp',
     data: () => {
         return {
             totalStock: 0,
-            people: ""
+            selStock: "" ,
+            checkbox1 : true,
+            curStock: "",
+            curStockCnt:0
         };
     },
     methods: {
         subDonate: function () {
             this.$parent.donate()
         },
-        readStock: function(){
+        readStock: function () {
             var stocklst = new Array()
-            var lst = Object.keys(stockJSON);            
-            Object.keys(lst).forEach(function(v){
-                stocklst.push({ name : lst[v] , symbol : stockJSON[lst[v]]});
+            var lst = Object.keys(stockJSON);
+            Object.keys(lst).forEach(function (v) {
+                stocklst.push({
+                    name: lst[v],
+                    symbol: stockJSON[lst[v]]
+                });
             })
-            this.people=stocklst
-        }
+            this.selStock = stocklst
+        },
+        checkStock: function (value) {
+            //여기 잔고 체크 로직 추가 
+            this.curStock = value.name;
+            this.curStockCnt = 10
+
+        },
     },
     mounted() {
         console.log("호출");
