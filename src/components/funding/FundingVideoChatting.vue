7743402c57a163816721a6ce0b868bf39c95a604
jaehyun kim <kimjaehyun9409@gmail.com>
2023-02-12 17:43:54 +0900

    참고 자료 주석 추가

--- src/components/funding/FundingVideoChatting.vue
+++ src/components/funding/FundingVideoChatting.vue
@@ -8,6 +8,10 @@
 
 <script>
 
+// [WebRTC 참고] 
+// 1. https://github.com/SeJonJ/Spring-WebSocket-WebRTC-Chatting/blob/master-Webrtc-jpa/src/main/resources/static/js/rtc/webrtc_client.js
+// 2. https://github.com/Benkoff/WebRTC-SS/blob/master/src/main/resources/static/js/webrtc_client.js
+
 let socket;
 
 // WebRTC variables
